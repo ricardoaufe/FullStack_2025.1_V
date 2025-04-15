b6885b1416e531ae1e401a2f07d7c0d8ed63be8c
Ricardo Ferreira <rasf0831@gmail.com>
2025-04-15 10:25:12 -0300

    animacao-end

--- LAB/animacao.js
+++ LAB/animacao.js
@@ -1,14 +1,40 @@
-class Circulo{
-    constructor(cor, [x, y, raio, angInicial, angFinal], borderCor){
+class Retangulo{
+    constructor(corLinha,cor,widthLinha,x,y,largura,altura){
+        this.corLinha = corLinha;
         this.cor = cor;
+        this.widthLinha = widthLinha;
         this.x = x;
         this.y = y;
-        this.raio = raio;
-        this.angInicial = angInicial;
-        this.angFinal = angFinal;
-        this.borderCor = borderCor;
+        this.largura = largura;
+        this.altura = altura;
     }
-    desenhe(ctx){
-        ctx.beginPath(); // colocar todos os parametros do arco
+    desenhe(contexto){
+        contexto.beginPath();
+        contexto.lineWidth = this.widthLinha;
+        contexto.fillStyle = this.cor;
+        contexto.strokeStyle = this.corLinha;
+        contexto.fillRect(this.x,this.y,this.largura,this.altura);
+        contexto.closePath();
     }
 }
+
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+let retangulo = new Retangulo('blue', 'red', 3, 200, 200, 20, 20);
+
+document.addEventListener('mousemove', function(evento){
+    let rect = canvas.getBoundingClientRect();
+    let x_mouse = evento.clientX - rect.left;
+    let y_mouse = evento.clientY - rect.top;
+
+    retangulo.x = x_mouse - retangulo.largura / 2;
+    retangulo.y = y_mouse - retangulo.altura / 2;
+    
+    retangulo.x = Math.min(Math.max(0, x_mouse - retangulo.largura / 2), canvas.width - retangulo.largura);
+    retangulo.y = Math.min(Math.max(0, y_mouse - retangulo.altura / 2), canvas.height - retangulo.altura);
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    retangulo.desenhe(ctx);
+});
+
